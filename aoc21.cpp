7d219c7dd7751de0997d67962a8f4282608930fb
miro salvagni <miro.salvagni@knorr-bremse.com>
2020-12-29 13:00:45 +0100

    Day 21

--- aoc21.cpp
+++ aoc21.cpp
@@ -1,10 +1,9 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <map>
-#include <numeric>
 #include <vector>
+#include <set>
 
 // "aocXY.extension" -> "aocXY.txt"
 #define PUZZLE_INPUT_FILENAME (std::string(__FILE__).substr(0, std::string(__FILE__).find('.') + 1) + "txt")
@@ -22,82 +21,130 @@ static auto readFile(const std::string filename)
   return ret;
 }
 
-std::vector<std::string> split_string(std::string x)
-{
-  std::istringstream iss(x);
-  std::vector<std::string> res;
-
-  std::string k;
-
-  while (iss >> k)
-    res.push_back(k);
-
-  return res;
-}
-
 struct food
 {
-  std::vector<std::string> ingredients;
-  std::vector<std::string> allergenes;
+  std::set<std::string> allergenes;
+  std::set<std::string> ingredients;
 
-  food() : ingredients(), allergenes() {}
+  food() : allergenes(), ingredients() {}
 
   friend std::istream &operator>>(std::istream &in, food &entry)
   {
     std::string word;
     bool allergenes = false;
-    bool done = false;
 
-    entry.ingredients.clear();
     entry.allergenes.clear();
+    entry.ingredients.clear();
 
-    do
-    {
-      if (!(in >> word))
-        return in;      
-
-      if (word[0] == '(') // (contains
+    while (in >> word)
+      if (word[0] == '(') // "(contains", skip this word
         allergenes = true;
       else if (allergenes)
       {
-        if (word[word.length()-1] == ')')
-        {
-          done = true;
-          word[word.length()-1] = '\0';
-        }
+        // remove last char: ',' or ')'
+        entry.allergenes.insert(word.substr(0, word.size()-1));
 
-        entry.allergenes.push_back(word);
+        if (word[word.length()-1] == ')')
+          return in;
       }
       else
-        entry.ingredients.push_back(word);
+        entry.ingredients.insert(word);
 
-    } while (!done);
-
-    return in;
-  }  
+    return in;      
+  }
 };
 
+
+void output(std::set<std::string> theset)
+{
+  for (auto x : theset)
+    std::cout << x << " ";
+  std::cout << std::endl;
+}
+
+auto getAllergenesMap(std::vector<food> foods)
+{
+  // map  allergene -> ingredient that has it
+  std::map<std::string, std::string> allergenes;
+
+  // all allergenes are unknown
+  for (auto f : foods)
+    for (auto a : f.allergenes)
+      allergenes[a] = "";
+
+  // till there is at least one associate
+  while (std::any_of(allergenes.begin(), allergenes.end(), [](auto x) { return x.second.empty(); }))
+    for (auto x : allergenes)
+      if (x.second.empty())
+      {
+        bool starting = true;
+        std::set<std::string> possibles;
+
+        // for all foods that contain that allergene
+        for (auto f : foods)
+          if (f.allergenes.count(x.first))
+          {
+            if (starting)
+            {
+              // first one, assign all ingredients
+              starting = false;
+              possibles = f.ingredients;
+            }
+            else
+            {
+              // another one, intersect
+              std::set<std::string> inter;
+              std::set_intersection(possibles.begin(), possibles.end(), f.ingredients.begin(), f.ingredients.end(), std::inserter(inter, inter.end()));
+              possibles = inter;
+            }
+          }
+
+        // remove the ones we already know
+        for (auto y : allergenes)
+          possibles.erase(y.second);
+
+        // if only one remains, we found a new association
+        if (possibles.size() == 1)
+          allergenes[x.first] = *possibles.begin();
+      }
+
+  return allergenes;
+}
 
 auto es1()
 {
   const auto foods = readFile<food>(PUZZLE_INPUT_FILENAME);
+  auto allergenes = getAllergenesMap(foods);
 
-  // for (auto f : foods)
-  // {
-  //   for (auto t : f.ingredients)
-  //     std::cout << t << " ";
-  //   std::cout << std::endl;
-  //   // std::cout << f.ingredients.size() << " " << f.allergenes.size() << std::endl;
-  // }
+  int count = 0;
 
-  return 0;
+  for (auto f : foods)
+    for (auto t : f.ingredients)
+      if (!std::any_of(allergenes.begin(), allergenes.end(), [&t](auto x) { return t == x.second; }))
+        count++;
+
+  return count;
 }
 
 auto es2()
 {
   const auto foods = readFile<food>(PUZZLE_INPUT_FILENAME);
+  auto allergenes = getAllergenesMap(foods);
 
-  return 0;
+  // get a sorted vector of all allergenes
+  std::vector<std::string> only_allerg;
+
+  for (auto x : allergenes)
+    only_allerg.push_back(x.first);
+
+  std::sort(only_allerg.begin(), only_allerg.end());
+
+  // get elements in order and concatenate them into result string
+  std::string ret = allergenes[only_allerg[0]];
+  for (uint i=1; i<only_allerg.size(); i++)
+    ret += "," + allergenes[only_allerg[i]];
+
+  return ret;
 }
 
 int main()
